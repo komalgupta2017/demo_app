cd79cf58f3d35ffcf9bbafe4b7ad4d9bbda683d8
Komal <komal2.gupta@paytm.com>
2021-01-16 16:41:46 +0530

    add gitignore

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+DemoApp::Application.config.secret_key_base = '95d5b6353a988ec09e39e546b6a87fb73e3b9e6c89891c3e11a75afe830d523add41a28f2407e6e8df79bf04f75039fa7ba918b1bdbf13d5a6e8a5108504935b'
